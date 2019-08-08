4912dfa65b7c75e53c3cb5da00c2b207b952b5f4
AKASH64 <53894266+AKASH64@users.noreply.github.com>
2019-08-08 13:05:56 +0530

    Create discount.c

---
+++ discount.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int price,i;
+int disprice;
+int dispercent;
+int disamount;
+i=price;
+scanf("%d%d",&price,i);
+for(i=price;i<0;i--){
+disamount=( (dispercent)*price )/100;
+disamount(price-disamount);
+}
+}
